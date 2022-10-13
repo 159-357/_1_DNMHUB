99a199540a29704b6f264964537b1439185686f9
Alp Eren TOKSOY <alperentoksoy95@gmail.com>
2022-10-14 01:34:11 +0300

    bu kursa yazılım amacımı belirttim2

--- src/gUN156/derstekrarı.java
+++ src/gUN156/derstekrarı.java
@@ -2,7 +2,7 @@ package gUN156;
 
 public class derstekrarı {
     public static void main(String[] args) {
-        System.out.println("yine merhaba dedik dünyaya");
+
 
         for (int i = 0; i < 3; i++) {
             System.out.println(" halledicem bu yazılım işini");
